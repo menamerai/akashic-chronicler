07acbf8ed16356d75b2bc5a14bb7b10a2d15af8a
menamerai <rphananh@gmail.com>
2024-01-31 20:53:51 -0500

    fix class misname

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
 export default function Card({ title, content, id, routePrefix }) {
     return (
-        <a class="w-72 h-72 flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg hover:shadow-green-500/20 transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] m-8 overflow-hidden text-ellipsis" href={`/${routePrefix}${id}`}>
-            <div class="p-4 md:p-5 h-full">
-                <h3 class="text-lg font-bold text-gray-800 dark:text-white text-center">
+        <a className="w-72 h-72 flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-lg hover:shadow-green-500/20 transition dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7] m-8 overflow-hidden text-ellipsis" href={`/${routePrefix}${id}`}>
+            <div className="p-4 md:p-5 h-full">
+                <h3 className="text-lg font-bold text-gray-800 dark:text-white text-center">
                     {title}
                 </h3>
-                <p class="mt-1 text-gray-800 dark:text-gray-400 line-clamp-[8]">
+                <p className="mt-1 text-gray-800 dark:text-gray-400 line-clamp-[8]">
                     {content}
                 </p>
             </div>
--- src/components/PersonalRecords.jsx
+++ src/components/PersonalRecords.jsx
@@ -45,8 +45,8 @@ export default function PersonalRecords() {
     if (!records) {
         return (
             <div className="w-full h-[70vh] flex justify-center items-center">
-                <div class="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-green-600 rounded-full" role="status" aria-label="loading">
-                    <span class="sr-only">Loading...</span>
+                <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-green-600 rounded-full" role="status" aria-label="loading">
+                    <span className="sr-only">Loading...</span>
                 </div>
             </div>
         );
