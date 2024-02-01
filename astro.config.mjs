bafa6dcd08811959dd2b4e52ac6053b2843d83ab
menamerai <rphananh@gmail.com>
2024-01-31 20:58:20 -0500

    added vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
@@ -7,4 +8,5 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: "server",
+  adapter: vercel(),
 });
